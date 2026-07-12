53fbb169b76cc20815f9d4bc39923cf9b55d6553
Your Name <your@email.com>
2026-07-12 18:28:09 +0530

    Add project documentation and code comments

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from fastapi import Request
+"""
+Photon - Real-Time WebSocket Stock Ticker
+
+Week 1:
+- FastAPI project setup
+- Static files configuration
+- HTML template configuration
+
+"""
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Static Files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Templates
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
