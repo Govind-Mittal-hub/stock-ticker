1296a4c6928d4407d6eba691c9817b2fa3606ffe
Your Name <your@email.com>
2026-07-12 14:14:07 +0530

    Configure static files and templates

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def home():
-    return {"message": "Photon Backend Running Successfully!"}
+templates = Jinja2Templates(directory="templates")
