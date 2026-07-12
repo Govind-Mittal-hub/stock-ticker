e7dfcaa98e55c12d6838b40cc6c8d4a00f15371c
Your Name <your@email.com>
2026-07-12 18:11:49 +0530

    Add route to serve home page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from fastapi.responses import HTMLResponse
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-templates = Jinja2Templates(directory="templates")
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
