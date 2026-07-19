90660dcd667b99663f9270f8d650f000a6c8d216
Your Name <your@email.com>
2026-07-19 15:26:00 +0530

    Add asynchronous stock data generator

--- data_streamer.py
+++ data_streamer.py
@@ -1,4 +1,5 @@
 # Mock Data Streamer for Photon
+
 import random
 import asyncio
 
@@ -9,8 +10,17 @@ stocks = {
     "AMZN": 210.00,
     "MSFT": 420.00
 }
+
+
 def update_prices():
     for stock in stocks:
         change = random.uniform(-3, 3)
         stocks[stock] += change
         stocks[stock] = round(stocks[stock], 2)
+
+
+async def generate_stock_data():
+    while True:
+        update_prices()
+        print(stocks)
+        await asyncio.sleep(1)
