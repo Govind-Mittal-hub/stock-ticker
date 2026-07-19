cb10a429e26e150f25340216f04b2aaf32836531
Your Name <your@email.com>
2026-07-19 14:48:40 +0530

    Save local changes before updating main

--- app.py
+++ app.py
@@ -14,16 +14,15 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-# Static Files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Templates
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html",
+        context={}
     )
